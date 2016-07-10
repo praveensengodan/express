c5a6a7b0e94e1fd56c6fd7e883703b9d965c565e
Praveen <praveensengodan@gmail.com>
2016-07-11 01:15:43 +0530

    pagination template added

--- public/js/directives/customTableDirective.js
+++ public/js/directives/customTableDirective.js
@@ -11,9 +11,40 @@ app.directive('customTable',function(){
     replace: 'true',
     restrict: 'E',
     link: function(scope){
+      scope.itemsPerPage = 5;
+      scope.currentPage = 1;
       scope.sort = function(sortBy){
         scope.options.sortBy = sortBy;
         scope.options.sortReverse = !scope.options.sortReverse;
+      }
+      scope.range = function() {
+        scope.pages = [1];     
+        if(scope.rows){
+        var length = scope.rows.length;
+        var itemsPerPage = scope.itemsPerPage;  
+          if(length > itemsPerPage){
+            for (var i = 1; i <= (length-5); i++) {
+              if(i % itemsPerPage === 0) {
+                scope.pages.push(scope.pages.length+1);
+              } 
+            }
+            (length % itemsPerPage) ? scope.pages.push(scope.pages.length+1) : null;
+          } 
+       } 
+        return scope.pages;
+      }
+      scope.prev = function(){
+        if(scope.currentPage > 1){
+          --scope.currentPage;
+        }
+      }
+      scope.next = function(){
+        if(scope.currentPage < scope.pages.length){
+          ++scope.currentPage;
+        }
+      }
+      scope.setPage = function(page){
+        scope.currentPage = page;
       }
     }
   }
